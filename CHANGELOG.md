00f85a68ad9fdf7357f963589c4c35d7c1b6895c
Luis Bastidas <luis@kabany.mx>
2025-04-15 11:23:12 -0600

    fix: handle null values

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [v2.1.1] 2025-04-15
+### Fix
+* Update `ExpressFailedResponse` argument's order and handle null values
+
 ## [v2.1.0] 2025-04-15
 ### Add
 * Add `ExpressFailedResponse` error to easy the error handling.
--- src/express.error.ts
+++ src/express.error.ts
@@ -7,21 +7,23 @@ export class ExpressFailedResponse extends CoreError {
   reason: string
   statusCode: number
 
-  constructor(code: number, message: string, errBody?: any, reason?: any, statusCode?: number) {
+  constructor(code: number, message: string, errBody?: any, statusCode?: number, reason?: any) {
     super();
     Object.setPrototypeOf(this, ExpressFailedResponse.prototype);
     this.name = this.constructor.name
     this.message = message;
     this.errBody = errBody;
     this.code = code;
-    if (reason instanceof Error) {
-      if (reason.stack) {
-        this.reason = reason.stack
+    if (reason != null) {
+      if (reason instanceof Error) {
+        if (reason.stack) {
+          this.reason = reason.stack
+        } else {
+          this.reason = reason.message
+        }
       } else {
-        this.reason = reason.message
+        this.reason = `${reason}`
       }
-    } else {
-      this.reason = `${reason}`
     }
     this.statusCode = statusCode || StatusCodes.INTERNAL_SERVER_ERROR
     if (Error.captureStackTrace) Error.captureStackTrace(this, ExpressFailedResponse);
